40de5e45e60eef76a9690a109d2148ea237dff03
Hardik03090407 <hardikrawat79@gmail.com>
2025-08-25 01:02:18 +0530

    Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
 import os
 import sys
-import pickle
+
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -19,14 +21,13 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-
-
+    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
-            model = list(models.values())[i]        
+            model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
@@ -35,7 +36,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-           # model.fit(X_train, y_train)  # Train model
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -45,10 +46,17 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = train_model_score
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
